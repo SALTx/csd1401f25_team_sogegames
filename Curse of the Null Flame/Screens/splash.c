ceb064731ed4465f01edc38af9cea2a622622efa
Saahil <29069313+SALTx@users.noreply.github.com>
2025-10-04 19:02:07 +0800

    Update splash.c

--- Curse of the Null Flame/Screens/splash.c
+++ Curse of the Null Flame/Screens/splash.c
@@ -2,29 +2,22 @@
 #include "../abstraction.h"
 #include "mainmenu.h"
 
-// Declare assets
 CP_Image digipen_logo;
-
-// Globals
 int digipen_logo_alpha;
 
 void splash_init(void) {
-	// Load digipen logo 
 	digipen_logo = CP_Image_Load("Assets/digipen_logo.png");
-
 	digipen_logo_alpha = 0;
 }
 
 void splash_update(void) {
-	// Black background
 	CP_Graphics_ClearBackground(BLACK);
 
 	// Draw the digipen logo
-	CP_Image_Draw(digipen_logo, 0.0f, 0.0f, CP_Image_GetWidth(digipen_logo), CP_Image_GetHeight(digipen_logo), digipen_logo_alpha++);
+	CP_Image_Draw(digipen_logo, WIDTH / 2, HEIGHT / 2, CP_Image_GetWidth(digipen_logo), CP_Image_GetHeight(digipen_logo), digipen_logo_alpha++);
 
-
-	// Skip to main menu by clicking
-	if (CP_Input_MouseClicked()) {
+	// Finish splash or if player skips
+	if (digipen_logo_alpha >= 255 || CP_Input_MouseClicked()) {
 		CP_Engine_SetNextGameState(mainmenu_init, mainmenu_update, mainmenu_exit);
 	}
 
