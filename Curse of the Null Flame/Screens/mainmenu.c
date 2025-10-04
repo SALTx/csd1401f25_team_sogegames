fa28413c3518c7aeee51a82a874535790836f720
Saahil <29069313+SALTx@users.noreply.github.com>
2025-10-04 19:02:05 +0800

    Update mainmenu.c

--- Curse of the Null Flame/Screens/mainmenu.c
+++ Curse of the Null Flame/Screens/mainmenu.c
@@ -1,11 +1,12 @@
 #include "cprocessing.h"
+#include "../abstraction.h"
 
 void mainmenu_init(void) {
 
 }
 
 void mainmenu_update(void) {
-
+	CP_Graphics_ClearBackground(CP_Color_Create(0, 255, 0, 255));
 }
 
 void mainmenu_exit(void) {
