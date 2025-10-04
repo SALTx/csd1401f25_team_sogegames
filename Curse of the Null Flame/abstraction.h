6a7cee28416f6ed6511fec44ecf1dd40c67883f8
Saahil <29069313+SALTx@users.noreply.github.com>
2025-10-04 18:33:46 +0800

    Added abstraction header

---
+++ Curse of the Null Flame/abstraction.h
@@ -0,0 +1,8 @@
+#ifndef ABSTRACTION_H
+#define ABSTRACTION_H
+
+// Colors
+#define BLACK CP_Color_Create(0, 0, 0, 255)
+#define WHITE CP_Color_Create(255, 255, 255, 255)
+
+#endif // ABSTRACTION_H
--- Curse of the Null Flame/main.c
+++ Curse of the Null Flame/main.c
@@ -1,38 +1,32 @@
 #include "cprocessing.h"
-
-CP_Image logo;
+#include "abstraction.h"
 
 void game_init(void)
 {
-	logo = CP_Image_Load("Assets/digipen_logo.png");
 	CP_Settings_ImageMode(CP_POSITION_CORNER);
 	CP_Settings_ImageWrapMode(CP_IMAGE_WRAP_CLAMP);
 
-	CP_System_SetWindowSize(CP_Image_GetWidth(logo), CP_Image_GetHeight(logo));
+	CP_System_SetWindowSize(1600, 900);
 }
 
 void game_update(void)
 {
-	CP_Graphics_ClearBackground(CP_Color_Create(0, 0, 0, 255));
-	CP_Image_Draw(logo, 0.f, 0.f, CP_Image_GetWidth(logo), CP_Image_GetHeight(logo), 255);
+	// Black background
+	CP_Graphics_ClearBackground(BLACK);
+	
+	// Quit with Button Q
 	if (CP_Input_KeyDown(KEY_Q))
 	{
 		CP_Engine_Terminate();
 	}
-
 }
 
 void game_exit(void)
 {
-	CP_Image_Free(logo);
 }
-
-
-
 
 int main(void)
 {
-	//CP_Engine_SetNextGameState(splash_screen_init, splash_screen_update, splash_screen_exit);
 	CP_Engine_SetNextGameState(game_init, game_update, game_exit);
 	CP_Engine_Run(0);
 	return 0;
