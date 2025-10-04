3532b4a4483aac90fc87250a8eccb72e37d96aa8
Saahil <29069313+SALTx@users.noreply.github.com>
2025-10-04 18:47:14 +0800

    Added main menu files

---
+++ Curse of the Null Flame/Screens/mainmenu.c
@@ -0,0 +1,13 @@
+#include "cprocessing.h"
+
+void mainmenu_init(void) {
+
+}
+
+void mainmenu_update(void) {
+
+}
+
+void mainmenu_exit(void) {
+
+}
---
+++ Curse of the Null Flame/Screens/mainmenu.h
@@ -0,0 +1,8 @@
+#ifndef MAINMENU_H
+#define MAINMENU_H
+
+void mainmenu_init(void);
+void mainmenu_update(void);
+void mainmenu_exit(void);
+
+#endif // MAINMENU_H
