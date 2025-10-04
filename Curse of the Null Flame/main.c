9ed18f8706395aa1a13d6d51772ab38d650d6c61
Saahil <29069313+SALTx@users.noreply.github.com>
2025-10-04 18:47:19 +0800

    Update main.c

--- Curse of the Null Flame/main.c
+++ Curse of the Null Flame/main.c
@@ -1,33 +1,20 @@
 #include "cprocessing.h"
 #include "abstraction.h"
+#include "Screens/splash.h"
 
-void game_init(void)
-{
-	CP_Settings_ImageMode(CP_POSITION_CORNER);
-	CP_Settings_ImageWrapMode(CP_IMAGE_WRAP_CLAMP);
-
-	CP_System_SetWindowSize(1600, 900);
-}
-
-void game_update(void)
-{
-	// Black background
-	CP_Graphics_ClearBackground(BLACK);
-	
-	// Quit with Button Q
-	if (CP_Input_KeyDown(KEY_Q))
-	{
-		CP_Engine_Terminate();
-	}
-}
-
-void game_exit(void)
-{
-}
+void main_init(void){}
+void main_update(void){}
+void main_exit(void){}
 
 int main(void)
 {
-	CP_Engine_SetNextGameState(game_init, game_update, game_exit);
+	// Essential configuration
+	CP_System_SetWindowSize(1600, 900);
+
+	// Images
+	CP_Settings_ImageMode(CP_POSITION_CORNER);
+
+	CP_Engine_SetNextGameState(splash_init, splash_update, splash_exit);
 	CP_Engine_Run(0);
 	return 0;
 }
