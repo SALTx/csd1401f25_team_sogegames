81a1c51b723602c0e91b163f4681d7806db7390c
Saahil <29069313+SALTx@users.noreply.github.com>
2025-10-04 19:02:02 +0800

    Update main.c

--- Curse of the Null Flame/main.c
+++ Curse of the Null Flame/main.c
@@ -10,11 +10,9 @@ int main(void)
 {
 	// Essential configuration
 	CP_System_SetWindowSize(1600, 900);
-
-	// Images
-	CP_Settings_ImageMode(CP_POSITION_CORNER);
+	CP_System_SetFrameRate(60.0f); // Set the framerate to 60 FPS for consistency
 
 	CP_Engine_SetNextGameState(splash_init, splash_update, splash_exit);
-	CP_Engine_Run(0);
+	CP_Engine_Run(1);
 	return 0;
 }
