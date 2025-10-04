6598f4999cfb07592577fa103ef8fc7fb7fe8c95
Saahil <29069313+SALTx@users.noreply.github.com>
2025-10-04 19:01:48 +0800

    Update abstraction.h

--- Curse of the Null Flame/abstraction.h
+++ Curse of the Null Flame/abstraction.h
@@ -5,4 +5,8 @@
 #define BLACK CP_Color_Create(0, 0, 0, 255)
 #define WHITE CP_Color_Create(255, 255, 255, 255)
 
+// Window
+#define WIDTH CP_System_GetWindowWidth()
+#define HEIGHT CP_System_GetWindowHeight()
+
 #endif // ABSTRACTION_H
